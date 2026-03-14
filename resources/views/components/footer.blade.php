0edce38de59055a3d0232d1f887b63b54affa7d4
Learn From Xavi <xaviadalid959@gmail.com>
2026-03-14 18:45:47 +0800

    Refine footer layout and remove newsletter section

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-<footer class="site-footer" id="contact">
+<footer class="site-footer" id="contact" role="contentinfo" itemscope itemtype="https://schema.org/Restaurant">
     <!-- Footer Top Wave -->
     <div class="footer-wave">
         <svg viewBox="0 0 1440 120" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -9,17 +9,18 @@
     
     <div class="footer-main">
         <div class="container">
+            <h2 class="visually-hidden">Footer Information</h2>
             <div class="row g-5">
                 <!-- Brand Column -->
                 <div class="col-lg-4">
                     <div class="footer-brand">
-                        <a href="#home" class="brand-link">
-                            <img src="/images/logo.png" alt="Pita Queen" style="height: 40px; width: auto;">
+                        <a href="#home" class="brand-link" itemprop="url" aria-label="Go to top">
+                            <img src="/images/logo.png" alt="Pita Queen logo" style="height: 40px; width: auto;" itemprop="logo" loading="lazy">
                             <span class="brand-text">
-                                <span class="brand-name">Pita Queen</span>
+                                <span class="brand-name" itemprop="name">Pita Queen</span>
                             </span>
                         </a>
-                        <p class="brand-description">
+                        <p class="brand-description" itemprop="description">
                             Experience the finest Mediterranean cuisine crafted with passion, 
                             premium ingredients, and time-honored recipes since 2010.
                         </p>
@@ -49,13 +50,15 @@
                 <div class="col-lg-2 col-md-4">
                     <div class="footer-links">
                         <h4 class="footer-title">Quick Links</h4>
-                        <ul class="link-list">
-                            <li><a href="#home">Home</a></li>
-                            <li><a href="#about">About Us</a></li>
-                            <li><a href="#menu">Our Menu</a></li>
-                            <li><a href="#locations">Locations</a></li>
-                            <li><a href="#testimonials">Reviews</a></li>
-                        </ul>
+                        <nav aria-label="Quick links">
+                            <ul class="link-list">
+                                <li><a href="#home">Home</a></li>
+                                <li><a href="#about">About Us</a></li>
+                                <li><a href="#menu">Our Menu</a></li>
+                                <li><a href="#locations">Locations</a></li>
+                                <li><a href="#testimonials">Reviews</a></li>
+                            </ul>
+                        </nav>
                     </div>
                 </div>
                 
@@ -63,13 +66,15 @@
                 <div class="col-lg-2 col-md-4">
                     <div class="footer-links">
                         <h4 class="footer-title">Menu</h4>
-                        <ul class="link-list">
-                            <li><a href="#menu">Signature Items</a></li>
-                            <li><a href="#menu">Wraps</a></li>
-                            <li><a href="#menu">Platters</a></li>
-                            <li><a href="#menu">Bowls</a></li>
-                            <li><a href="#menu">Sides & Drinks</a></li>
-                        </ul>
+                        <nav aria-label="Menu links">
+                            <ul class="link-list">
+                                <li><a href="#menu">Signature Items</a></li>
+                                <li><a href="#menu">Wraps</a></li>
+                                <li><a href="#menu">Platters</a></li>
+                                <li><a href="#menu">Bowls</a></li>
+                                <li><a href="#menu">Sides & Drinks</a></li>
+                            </ul>
+                        </nav>
                     </div>
                 </div>
                 
@@ -78,13 +83,13 @@
                     <div class="footer-contact">
                         <h4 class="footer-title">Contact Us</h4>
                         
-                        <div class="contact-list">
+                        <address class="contact-list" itemprop="address" itemscope itemtype="https://schema.org/PostalAddress">
                             <div class="contact-item">
                                 <div class="contact-icon">
                                     <i class="bi bi-geo-alt"></i>
                                 </div>
                                 <div class="contact-text">
-                                    <span>{{ $contactSettings['contact_address_line1'] }}</span>
+                                    <span itemprop="streetAddress">{{ $contactSettings['contact_address_line1'] }}</span>
                                     <span>{{ $contactSettings['contact_address_line2'] }}</span>
                                 </div>
                             </div>
@@ -94,7 +99,7 @@
                                     <i class="bi bi-telephone"></i>
                                 </div>
                                 <div class="contact-text">
-                                    <a href="tel:{{ preg_replace('/[^0-9+]/', '', $contactSettings['contact_phone']) }}">{{ $contactSettings['contact_phone'] }}</a>
+                                    <a href="tel:{{ preg_replace('/[^0-9+]/', '', $contactSettings['contact_phone']) }}" itemprop="telephone">{{ $contactSettings['contact_phone'] }}</a>
                                 </div>
                             </div>
                             
@@ -103,7 +108,7 @@
                                     <i class="bi bi-envelope"></i>
                                 </div>
                                 <div class="contact-text">
-                                    <a href="mailto:{{ $contactSettings['contact_email'] }}">{{ $contactSettings['contact_email'] }}</a>
+                                    <a href="mailto:{{ $contactSettings['contact_email'] }}" itemprop="email">{{ $contactSettings['contact_email'] }}</a>
                                 </div>
                             </div>
                             
@@ -112,33 +117,10 @@
                                     <i class="bi bi-clock"></i>
                                 </div>
                                 <div class="contact-text">
-                                    <span>{{ $contactSettings['contact_hours'] }}</span>
+                                    <span itemprop="openingHours">{{ $contactSettings['contact_hours'] }}</span>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-            <!-- Newsletter -->
-            <div class="footer-newsletter">
-                <div class="row align-items-center">
-                    <div class="col-lg-6">
-                        <div class="newsletter-text">
-                            <h4>Subscribe to Our Newsletter</h4>
-                            <p>Get exclusive offers, new menu updates, and special promotions delivered to your inbox.</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <form class="newsletter-form">
-                            <div class="input-group">
-                                <input type="email" class="form-control" placeholder="Enter your email" required>
-                                <button type="submit" class="btn btn-golden">
-                                    <span>Subscribe</span>
-                                    <i class="bi bi-send ms-2"></i>
-                                </button>
-                            </div>
-                        </form>
+                        </address>
                     </div>
                 </div>
             </div>
@@ -155,11 +137,11 @@
                     </p>
                 </div>
                 <div class="col-md-6">
-                    <div class="footer-bottom-links">
+                    <nav class="footer-bottom-links" aria-label="Legal links">
                         <a href="#">Privacy Policy</a>
                         <a href="#">Terms of Service</a>
                         <a href="#">Cookie Policy</a>
-                    </div>
+                    </nav>
                 </div>
             </div>
         </div>
